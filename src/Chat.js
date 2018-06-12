3b16c06b6d91cc13e6fac9feefbd1bf1a99950d0
Rionna <Rionna@Rionnas-MacBook-Air.local>
2018-06-12 11:13:19 -0400

    Add a (hard-coded) message when the form is submitted

--- src/Chat.js
+++ src/Chat.js
@@ -5,23 +5,34 @@ import MessageList from './MessageList'
 import MessageForm from './MessageForm'
 
 class Chat extends Component {
-    constructor(){
-        super()
+  constructor() {
+    super()
 
-        this.state = {
-            messages: [
-                { id: 1, userName: 'dstrus', body: 'Chatting up a storm, yo!' },
-                { id: 2, userName: 'dpalazzo', body: 'This guy is so annoying. I hate my job.' }
-            ]
-        }
+    this.state = {
+      messages: [
+        { id: 1, userName: 'dstrus', body: 'Chatting up a storm, yo!' },
+        { id: 2, userName: 'dpalazzo', body: 'This guy is so annoying. I hate my job.' },
+      ],
     }
+  }
+
+  addMessage = () => {
+    const messages = [...this.state.messages]
+    messages.push({
+      id: Date.now(),
+      userName: 'Ferris',
+      body: 'Pick me! Pick me!',
+    })
+
+    this.setState({ messages })
+  }
 
   render() {
     return (
       <div className="Chat">
-        <ChatHeader/>
-        <MessageList messages={this.state.messages}/>
-        <MessageForm/>
+        <ChatHeader />
+        <MessageList messages={this.state.messages} />
+        <MessageForm addMessage={this.addMessage} />
       </div>
     )
   }
