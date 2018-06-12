fa9002e424737a8027dbf497c806bfa853ffab50
Rionna <Rionna@Rionnas-MacBook-Air.local>
2018-06-12 11:53:45 -0400

    Style Sidebar

--- src/Main.js
+++ src/Main.js
@@ -7,7 +7,6 @@ class Main extends Component {
   render() {
     return (
       <div className="Main" style={styles}>
-        Main
         <Sidebar user={this.props.user}/>
         <Chat user={this.props.user}/>
       </div>
