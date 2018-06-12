f7ba962b6ae49560869c9116be87b01b767796e2
Rionna <Rionna@Rionnas-MacBook-Air.local>
2018-06-12 10:14:28 -0400

    Add Main, Sidebar, and Chat component;  Style the body and main

---
+++ src/Chat.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import ChatHeader from './ChatHeader'
+
+class Chat extends Component {
+  render() {
+    return (
+      <div className="Chat">
+        <ChatHeader/>
+      </div>
+    )
+  }
+}
+
+export default Chat
---
+++ src/Main.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+import Sidebar from './Sidebar'
+import Chat from './Chat'
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="Main" style={styles}>
+        Main
+        <Sidebar/>
+        <Chat/>
+      </div>
+    )
+  }
+}
+
+const styles = {
+    display: 'flex',
+    alignItems: 'stretch',
+    height: '100vh',
+}
+
+export default Main
